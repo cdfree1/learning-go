390cbf8283dcc3b68d76e0babe276a0bdbb5a436
cdfree1 <cdfree@unc.edu>
2026-02-08 12:51:08 -0500

    Fully implemented hello with tests

--- hello.go
+++ hello.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 )
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		return englishHelloPrefix + "World"
+	} else {
+		return englishHelloPrefix + name
+	}
 	
 }
 
